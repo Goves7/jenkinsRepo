0f750dc39eec8b4c1b14ac807f68e0c9447610f8
Collin <Collin@DESKTOP-1DRG44F>
2023-12-12 15:52:44 +0530

    Jenkins

--- TimeSheetManagementToolProject/src/main/java/genericPackage/CustomeListener.java
+++ TimeSheetManagementToolProject/src/main/java/genericPackage/CustomeListener.java
@@ -7,47 +7,48 @@ import org.testng.Reporter;
 
 public class CustomeListener extends BaseTest implements ITestListener {
 
-	@Override
 	public void onTestStart(ITestResult result) {
 		Reporter.log("onTestStart",true);
 	}
 
-	@Override
+	
 	public void onTestSuccess(ITestResult result) {
 		Reporter.log("onTestSuccess",true);
 	}
 
-	@Override
+	
 	public void onTestFailure(ITestResult result) {
 	
 		Reporter.log("onTestFailure",true);
 		String failedMethodName = result.getMethod().getMethodName();
 		Reporter.log("Method Failed Taken Screenshots!!!",true);
-		failedSsMethod(failedMethodName);
+	    failedSsMethod(failedMethodName);
 		
 	}
 
-	@Override
+
+
+	
 	public void onTestSkipped(ITestResult result) {
 		Reporter.log("onTestSkipped",true);
 	}
 
-	@Override
+
 	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
 		
 	}
 
-	@Override
+	
 	public void onTestFailedWithTimeout(ITestResult result) {
 	
 	}
 
-	@Override
+	
 	public void onStart(ITestContext context) {
 		Reporter.log("onStart",true);
 	}
 
-	@Override
+
 	public void onFinish(ITestContext context) {
 		Reporter.log("onFinish",true);
 	}
